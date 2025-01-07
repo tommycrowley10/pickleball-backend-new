748c14d8792530b57e21c3fc6b8d4ec0afe8a0e0
tommycrowley10 <tommycrowley10@gmail.com>
2025-01-07 14:44:19 -0600

    Add initial seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(first_name: "John", last_name: "Doe", email: "john.doe@example.com", username: "johndoe23", password: "SecurePass123!")
+User.create(first_name: "Jane", last_name: "Smith", email: "jane.smith@example.com", username: "jsmith90", password: "P@ssw0rd!")
+User.create(first_name: "Jane", last_name: "Smith", email: "jane.smith@example.com", username: "jsmith90", password: "P@ssw0rd!")
+User.create(first_name: "Emily", last_name: "Brown", email: "emily.brown@example.com", username: "emily_brown77", password: "Brownie_2024")
+User.create(first_name: "Chris", last_name: "Williams", email: "chris.williams@example.com", username: "chris_will", password: "Cr!s$W12")
+User.create(first_name: "Olivia", last_name: "Davis", email: "olivia.davis@example.com", username: "olivd21", password: "Oliv@2024")
